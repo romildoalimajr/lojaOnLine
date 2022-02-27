d08a84c850d8dc3e0df6351f5eae516868871004
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2022-02-27 09:23:12 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Loja On Line</title>
+        <link rel="shortcut icon" type="img/x-icon" href="./img/kalangos.ico">
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+        />
+        <link rel="stylesheet" href="css/style.css" />
+        
+    </head>
+    <body>
+        <h1>loja on line</h1>
+
+
+        <script src="js/script.js"></script>
+    </body>
+</html>
